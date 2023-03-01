414f217167b0de3e91fcb092e32b69ff871b2f1a
o-shvarts <119028609+o-shvarts@users.noreply.github.com>
2023-03-01 23:41:35 +0300

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+
+
+class get_price:
+    @staticmethod
+    def convert(base: str, quote: str, amount: str):
+        #  Ошибку с количеством переменных вынес в основной код, так же решил, что ввод одинаковых валют для конвертации
+        #  некритичен, так как в этом случае бот отрабатывает корректно
+
+
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_quote = json.loads(r.content)[keys[quote]]
+        return total_quote
+
